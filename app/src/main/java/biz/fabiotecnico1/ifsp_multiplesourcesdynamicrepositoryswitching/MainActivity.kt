7cbd93db11f1aef12177c48e2e2e95c2c85f52d9
Fábio Luís <ml@fabiotecnico1.biz>
2026-06-09 17:06:57 -0300

    chore: configuração inicial do projeto com .gitignore

---
+++ app/src/main/java/biz/fabiotecnico1/ifsp_multiplesourcesdynamicrepositoryswitching/MainActivity.kt
@@ -0,0 +1,47 @@
+package biz.fabiotecnico1.ifsp_multiplesourcesdynamicrepositoryswitching
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import biz.fabiotecnico1.ifsp_multiplesourcesdynamicrepositoryswitching.ui.theme.IfspMultipleSourcesDynamicRepositorySwitchingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IfspMultipleSourcesDynamicRepositorySwitchingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IfspMultipleSourcesDynamicRepositorySwitchingTheme {
+        Greeting("Android")
+    }
+}
